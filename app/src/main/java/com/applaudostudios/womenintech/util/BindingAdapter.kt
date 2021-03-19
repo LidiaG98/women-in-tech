38f83a6d5f11bf0266ea116fb3ef8a0b03ecbea0
Lidia González <lidiagp98@gmail.com>
2021-03-19 02:04:16 -0600

    :lipstick: Landscape variations and final design

--- app/src/main/java/com/applaudostudios/womenintech/util/BindingAdapter.kt
+++ app/src/main/java/com/applaudostudios/womenintech/util/BindingAdapter.kt
@@ -1,12 +1,15 @@
 package com.applaudostudios.womenintech.util
 
+import android.view.View
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.airbnb.lottie.LottieAnimationView
 import com.applaudostudios.womenintech.R
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 
-private const val IMAGES_BASE_URL = "https://raw.githubusercontent.com/LidiaG98/pictures-women-in-tech/main/Pictures"
+private const val IMAGES_BASE_URL =
+    "https://raw.githubusercontent.com/LidiaG98/pictures-women-in-tech/main/Pictures"
 
 @BindingAdapter("loadImage")
 fun loadImage(showImg: ImageView, imageUrl: String?) {
@@ -18,5 +21,16 @@ fun loadImage(showImg: ImageView, imageUrl: String?) {
             .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
             .error(R.drawable.ic_flower)
             .into(showImg)
+    }
+}
+
+@BindingAdapter("flag")
+fun setFlag(flagImage: ImageView, country: String?) {
+    country?.let {
+        when (country) {
+            "USA" -> flagImage.setImageResource(R.drawable.ic_usa)
+            "Austria" -> flagImage.setImageResource(R.drawable.ic_austria)
+            "England" -> flagImage.setImageResource(R.drawable.ic_united_kingdom)
+        }
     }
 }
